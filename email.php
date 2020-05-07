dc794258e3864a56ab8606457ec7e3f5d6ef1b8e
marcoshmarcarini <40902689+marcoshmarcarini@users.noreply.github.com>
2020-05-07 15:24:25 -0300

    Ultra update
    Upgrade no formulário, corrigindo inúmeros erros.

---
+++ email.php
@@ -0,0 +1,41 @@
+<?php
+
+if(isset($_POST['email']) && !empty($_POST['email'])){
+
+$nome = addslashes($_POST['nome']);
+$sobrenome = addslashes($_POST['sobrenome']);
+$cpf = addslashes($_POST['cpf']);
+$endereco = addslashes($_POST['endereco']);
+$cidade = addslashes($_POST['cidade']);
+$estado = addslashes($_POST['estado']);
+$email = addslashes($_POST['email']);
+
+}
+$to = "marcoshmarcarini@hotmail.com";
+$subject = "Contato - Pintores";
+$body = "Nome: ".$nome." ".$sobrenome."\r\n".
+        "CPF: ".$cpf."\r\n".
+        "Endereço: ".$endereco."\r\n".
+        "Cidade: ".$cidade."\r\n".
+        "Estado: ".$estado."\r\n".
+        "E-mail: ".$email;
+
+$header = "From:mdtintas@github.io"."\r\n"."Reply-To: ".$email."\r\n"."X=Mailer:PHP/".phpversion();
+
+if(mail($to,$subject,$body,$header)){
+
+    echo("Email enviado com sucesso!");
+
+}else{
+    echo("O Email não pode ser enviado.");
+}
+
+
+
+
+
+
+
+
+
+?>
